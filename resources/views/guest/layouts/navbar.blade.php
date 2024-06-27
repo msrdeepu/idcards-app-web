69361278537b0286d9593ec9fdcb3ab1e4cef4b4
sandeep <mspreddy2021@gmail.com>
2024-06-27 08:04:57 +0530

    nav-bar

--- resources/views/guest/layouts/navbar.blade.php
+++ resources/views/guest/layouts/navbar.blade.php
@@ -5,8 +5,11 @@
               <a href="index.html">
                   <img style="height: 55px" src="{{ asset('website/logos/smidpng.png') }}" alt="">
               </a>
+
+
+
               <div class="ms-auto d-flex align-items-center">
-                  <small class="ms-4"><i class="fa fa-map-marker-alt me-3"></i>123 Street, New York, USA</small>
+                  {{-- <small class="ms-4"><i class="fa fa-map-marker-alt me-3"></i>123 Street, New York, USA</small>
                   <small class="ms-4"><i class="fa fa-envelope me-3"></i>info@example.com</small>
                   <small class="ms-4"><i class="fa fa-phone-alt me-3"></i>+012 345 67890</small>
                   <div class="ms-3 d-flex">
@@ -17,14 +20,55 @@
                       <a class="btn btn-sm-square btn-light text-primary rounded-circle ms-2" href=""><i
                               class="fab fa-linkedin-in"></i></a>
                   </div>
+                   --}}
+
+                  <nav class="navbar navbar-expand-lg  navbar-light p-lg-0">
+                      <a href="index.html" class="navbar-brand d-lg-none">
+                          <img style="height: 55px" src="{{ asset('website/logos/smidpng.png') }}" alt="">
+                      </a>
+                      <button type="button" class="navbar-toggler me-0" data-bs-toggle="collapse"
+                          data-bs-target="#navbarCollapse">
+                          <span class="navbar-toggler-icon"></span>
+                      </button>
+                      <div class="collapse navbar-collapse" id="navbarCollapse">
+                          <div class="navbar-nav">
+                              <a href="index.html" class="nav-item nav-link active">Home</a>
+                              <a href="service.html" class="nav-item nav-link">Products & Services</a>
+                              <a href="project.html" class="nav-item nav-link">Blog</a>
+                              <a href="about.html" class="nav-item nav-link">About</a>
+                              <a href="contact.html" class="nav-item nav-link">Contact</a>
+
+                              {{-- <div class="nav-item dropdown">
+                                <a href="#" class="nav-link dropdown-toggle" data-bs-toggle="dropdown">Pages</a>
+                                <div class="dropdown-menu bg-light rounded-0 rounded-bottom m-0">
+                                    <a href="feature.html" class="dropdown-item">Features</a>
+                                    <a href="team.html" class="dropdown-item">Our Team</a>
+                                    <a href="testimonial.html" class="dropdown-item">Testimonial</a>
+                                    <a href="quote.html" class="dropdown-item">Quotation</a>
+                                    <a href="404.html" class="dropdown-item">404 Page</a>
+                                </div>
+                            </div> --}}
+
+                          </div>
+
+                      </div>
+                      <div class="ms-3 d-flex">
+                          <a class="btn btn-sm-square btn-light text-primary rounded-circle ms-2" href=""><i
+                                  class="fab fa-facebook-f"></i></a>
+                          <a class="btn btn-sm-square btn-light text-primary rounded-circle ms-2" href=""><i
+                                  class="fab fa-twitter"></i></a>
+                          <a class="btn btn-sm-square btn-light text-primary rounded-circle ms-2" href=""><i
+                                  class="fab fa-linkedin-in"></i></a>
+                      </div>
+                  </nav>
               </div>
           </div>
       </div>
   </div>
   <!-- Topbar End -->
 
-  <!-- Navbar Start -->
-  <div class="container-fluid bg-white sticky-top">
+  <!-- mobile Navbar Start -->
+  <div class="container-fluid bg-white sticky-top d-md-none">
       <div class="container">
           <nav class="navbar navbar-expand-lg bg-white navbar-light p-lg-0">
               <a href="index.html" class="navbar-brand d-lg-none">
